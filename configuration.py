a2efc1a65f14709ce8e908a8b40ff156f3c24d3a
Milos Vasic <milos85vasic@gmail.com>
2019-03-02 16:35:04 +0100

    Mail Server Factory: Postfix and Dovecot source code download links added.

---
+++ configuration.py
@@ -0,0 +1,6 @@
+from os.path import expanduser
+from Toolkit.configuration import *
+
+postfix_source = "http://mirror.lhsolutions.nl/postfix-release/official/postfix-3.4.0.tar.gz"
+dovecot_source = "https://www.dovecot.org/releases/2.3/dovecot-2.3.4.1.tar.gz"
+
