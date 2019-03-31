424ec3024c7fe9ef945b2efe194d5063ee5274f0
Milos Vasic <milos85vasic@gmail.com>
2019-03-31 17:15:46 +0200

    Mail Server Factory: Working on factory script.

--- factory.py
+++ factory.py
@@ -11,8 +11,7 @@ def user_home():
 
 def run_factory():
     dovecot_configuration = "./configure --prefix=" + user_home() + "/" + dovecot + " --with-sql --with-mysql " + \
-                            "--with-shadow --with-pam --with-nss --with-ldap" + \
-                            " --with-gssapi --with-vpopmail"
+                            "--with-shadow --with-pam --with-nss --with-gssapi --with-vpopmail"
 
     postfix_configuration = "./configure --prefix=" + user_home() + "/" + postfix + " "  # <- TODO.
 
