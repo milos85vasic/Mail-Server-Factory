2779a1ab42270110fa3a0dd839baef691baa5dfb
Milos Vasic <milos85vasic@gmail.com>
2019-03-31 15:25:42 +0200

    Mail Server Factory: Working on factory script.

--- factory.py
+++ factory.py
@@ -8,3 +8,14 @@ account = getpass.getuser()
 def user_home():
     return get_home_directory_path(account)
 
+
+def run_factory():
+    steps = [
+
+    ]
+
+    run(steps)
+
+
+if __name__ == '__main__':
+    run_factory()
