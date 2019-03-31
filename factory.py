eb095e993529386aeb8213ff4dcd0d8265fa84c4
Milos Vasic <milos85vasic@gmail.com>
2019-03-31 15:30:00 +0200

    Mail Server Factory: Working on factory script.

--- factory.py
+++ factory.py
@@ -11,7 +11,83 @@ def user_home():
 
 def run_factory():
     steps = [
+        run_as_su(
+            concatenate(
+                "yum localinstall -y --nogpgcheck " + rpm_fusion_free + " " + rpm_fusion_non_free,
+                get_yum_group("Development Tools"),
+                get_yum(  # TODO: Remove unused dependencies.
+                    "epel-release",
+                    "openssl-devel",
+                    "gcc",
+                    "make",
+                    "cmake",
+                    "automake",
+                    "libtool",
+                    "wget",
+                    "git",
+                    "libxml2",
+                    "libxml2-devel",
+                    "ncurses-devel",
+                    "lynx",
+                    "links",
+                    "autoconf",
+                    "re2c",
+                    "bzip2-devel",
+                    "libcurl-devel",
+                    "libicu-devel",
+                    "gcc-c++",
+                    "libmcrypt-devel",
+                    "libwebp-devel"
+                ),
+                add_to_group(account, mail_server_factory_group),
+                mkdir(content_dir_path(user_home())),
+                chown(account, content_dir_path(user_home())),
+                chgrp(account, content_dir_path(user_home())),
+                run_as_user(
+                    account,
+                    concatenate(
+                        cd("~"),
 
+                        # TODO:
+                        # mkdir(apache_home),
+                        # mkdir(apache_home + "/" + apache_vhosts_directory),
+                        # mkdir(php_home),
+                        # mkdir(mysql_home),
+                        # cp(
+                        #     content_dir_matrix_path(user_home()),
+                        #     content_dir_path(user_home())
+                        # ),
+                        # cp(
+                        #     content_dir_matrix_path_php(user_home()),
+                        #     content_dir_path(user_home())
+                        # ),
+                        # wget(apache_download, destination=(home + "/")),
+                        # extract(apache_extract, destination=home),
+                        # cd(apache_extracted),
+                        # "./configure --prefix=" + apache_home,
+                        # "make",
+                        # "make install",
+                        # cd("~"),
+                        # rm(apache_extracted),
+                        # rm(apache_tar_gz)
+                    )
+                ),
+
+                # TODO:
+                # cd(home + "/" + apache_factory),
+                # python(brotli_installation_script, account),
+                # run_as_user(
+                #     account,
+                #     concatenate(
+                #         cd(user_home() + "/" + apache_factory),
+                #         python(password_protect_script),
+                #         python(mysql_installation_script, account),
+                #         python(php_installation_script, account),
+                #         python(distribution_script)
+                #     )
+                # )
+            )
+        )
     ]
 
     run(steps)
