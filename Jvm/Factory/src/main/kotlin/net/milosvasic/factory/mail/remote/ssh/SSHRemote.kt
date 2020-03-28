1a6778dbac56ab01627786c79d5bc10bd8fffde1
Milos Vasic <milos85vasic@gmail.com>
2020-03-28 15:39:50 +0100

    To string.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/remote/ssh/SSHRemote.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/remote/ssh/SSHRemote.kt
@@ -2,7 +2,12 @@ package net.milosvasic.factory.mail.remote.ssh
 
 import net.milosvasic.factory.mail.remote.Remote
 
-data class SSHRemote(
+class SSHRemote(
     val port: Int,
     val username: String
-) : Remote()
+) : Remote() {
+
+    override fun toString(): String {
+        return "SSHRemote(port=$port, username='$username', host='$host')"
+    }
+}
