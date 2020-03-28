c83f69a06d43a83cbd25500b5a4e068303107533
Milos Vasic <milos85vasic@gmail.com>
2020-03-28 17:51:24 +0100

    Installer.

---
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/Installer.kt
@@ -0,0 +1,46 @@
+package net.milosvasic.factory.mail.component
+
+import net.milosvasic.factory.mail.common.Installation
+import net.milosvasic.factory.mail.common.Notifying
+import net.milosvasic.factory.mail.common.Subscription
+import net.milosvasic.factory.mail.operation.OperationResult
+import net.milosvasic.factory.mail.operation.OperationResultListener
+
+class Installer(private val installations: List<Installation>) :
+    Component(),
+    Installation,
+    Subscription<OperationResultListener>,
+    Notifying<OperationResult> {
+
+    private val subscribers = mutableSetOf<OperationResultListener>()
+
+    override fun install() {
+
+        installations.forEach {
+            it.install()
+        }
+    }
+
+    override fun uninstall() {
+
+        installations.forEach {
+            it.uninstall()
+        }
+    }
+
+    override fun subscribe(what: OperationResultListener) {
+        subscribers.add(what)
+    }
+
+    override fun unsubscribe(what: OperationResultListener) {
+        subscribers.remove(what)
+    }
+
+    override fun notify(data: OperationResult) {
+        val iterator = subscribers.iterator()
+        while (iterator.hasNext()) {
+            val listener = iterator.next()
+            listener.onOperationPerformed(data)
+        }
+    }
+}
