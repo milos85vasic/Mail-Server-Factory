b5c6d13bb9249cdb2ac56cbb00def55dccc5043e
Milos Vasic <milos85vasic@gmail.com>
2020-03-29 21:25:54 +0200

    Obtaining host OS information.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/os/OSType.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/os/OSType.kt
@@ -4,6 +4,7 @@ enum class OSType(val osName: String) {
 
     CENTOS("CentOS"),
     UBUNTU("Ubuntu"),
+    DEBIAN("Debian"),
     FEDORA("Fedora"),
     REDHAT("RedHat"),
     UNKNOWN("Unknown"),
--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/os/OperatingSystem.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/os/OperatingSystem.kt
@@ -1,5 +1,7 @@
 package net.milosvasic.factory.mail.os
 
+import net.milosvasic.factory.mail.log
+
 
 data class OperatingSystem(
     private var name: String = "System unknown",
@@ -7,9 +9,35 @@ data class OperatingSystem(
 ) {
 
     fun parseAndSetSystemInfo(data: String) {
-
-        // TODO:
-
+        val osLineString = "Operating System:"
+        val lines = data.split("\n")
+        lines.forEach {
+            if (it.contains(osLineString)) {
+                name = it.replace(osLineString, "").trim()
+                if (name.toLowerCase().contains(OSType.CENTOS.osName.toLowerCase())) {
+                    type = OSType.CENTOS
+                    return
+                }
+                if (name.toLowerCase().contains(OSType.FEDORA.osName.toLowerCase())) {
+                    type = OSType.FEDORA
+                    return
+                }
+                if (name.toLowerCase().contains(OSType.REDHAT.osName.toLowerCase())) {
+                    type = OSType.REDHAT
+                    return
+                }
+                if (name.toLowerCase().contains(OSType.UBUNTU.osName.toLowerCase())) {
+                    type = OSType.UBUNTU
+                    return
+                }
+                if (name.toLowerCase().contains(OSType.DEBIAN.osName.toLowerCase())) {
+                    type = OSType.DEBIAN
+                    return
+                }
+                type = OSType.GENERIC
+                return
+            }
+        }
     }
 
     fun getName() = name
