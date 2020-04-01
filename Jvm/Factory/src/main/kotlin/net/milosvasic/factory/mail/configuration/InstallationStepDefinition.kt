4c64e8109a31fdfb6c16bf96351c394fa6de58d0
Milos Vasic <milos85vasic@gmail.com>
2020-04-01 17:56:40 +0200

    Working on complex installations.

---
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/InstallationStepDefinition.kt
@@ -0,0 +1,6 @@
+package net.milosvasic.factory.mail.configuration
+
+data class InstallationStepDefinition(
+    val type: String,
+    val value: String
+)
--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/SoftwareConfigurationItem.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/SoftwareConfigurationItem.kt
@@ -1,5 +1,7 @@
 package net.milosvasic.factory.mail.configuration
 
 data class SoftwareConfigurationItem(
- val name: String
+    val name: String,
+    val version: String,
+    val installationSteps: Map<String, List<InstallationStepDefinition>>
 )
