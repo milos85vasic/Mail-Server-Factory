910ea89a431f4ba5a5b3f39d4dbcab24d68dee27
Milos Vasic <milos85vasic@gmail.com>
2020-04-01 18:51:42 +0200

    Working on complex installations.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/InstallationStepFactory.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/InstallationStepFactory.kt
@@ -1,6 +1,8 @@
 package net.milosvasic.factory.mail.component.installer.step
 
 import net.milosvasic.factory.mail.common.ObtainParametrized
+import net.milosvasic.factory.mail.component.packaging.item.Group
+import net.milosvasic.factory.mail.component.packaging.item.Package
 import net.milosvasic.factory.mail.configuration.InstallationStepDefinition
 
 class InstallationStepFactory : ObtainParametrized<InstallationStepDefinition, InstallationStep> {
@@ -15,11 +17,20 @@ class InstallationStepFactory : ObtainParametrized<InstallationStepDefinition, I
         when (definition.type) {
             InstallationStepType.PACKAGE_GROUP.type -> {
 
+                val group = Group(definition.value)
+                return PackageManagerInstallationStep(listOf(group))
             }
             InstallationStepType.PACKAGES.type -> {
 
+                val packages = mutableListOf<Package>()
+                val split = definition.value.split(",")
+                split.forEach {
+                    packages.add(Package(it.trim()))
+                }
+                return PackageManagerInstallationStep(packages)
             }
             InstallationStepType.COMMAND.type -> {
+
 
             }
         }
