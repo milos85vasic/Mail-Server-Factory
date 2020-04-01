acc65e5dbbb395efb2f078174ba4a8b1a10b6a1a
Milos Vasic <milos85vasic@gmail.com>
2020-04-01 15:48:50 +0200

    Removing unused imports.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/Installer.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/Installer.kt
@@ -3,17 +3,14 @@ package net.milosvasic.factory.mail.component.installer
 import net.milosvasic.factory.mail.common.Notifying
 import net.milosvasic.factory.mail.common.Subscription
 import net.milosvasic.factory.mail.common.busy.Busy
-import net.milosvasic.factory.mail.common.busy.BusyWorker
 import net.milosvasic.factory.mail.component.Component
 import net.milosvasic.factory.mail.component.Initialization
-import net.milosvasic.factory.mail.component.SystemComponent
 import net.milosvasic.factory.mail.component.Termination
 import net.milosvasic.factory.mail.component.packaging.PackageInstaller
 import net.milosvasic.factory.mail.component.packaging.PackageInstallerInitializationOperation
 import net.milosvasic.factory.mail.configuration.SoftwareConfiguration
 import net.milosvasic.factory.mail.operation.OperationResult
 import net.milosvasic.factory.mail.operation.OperationResultListener
-import net.milosvasic.factory.mail.remote.Connection
 import net.milosvasic.factory.mail.remote.ssh.SSH
 
 class Installer(
