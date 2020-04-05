944d9908d3ccf623754372dd0f59993e08356d69
Milos Vasic <milos85vasic@gmail.com>
2020-04-05 18:30:21 +0200

    MSF-65 - Working on reboot operation.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/reboot/Reboot.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/reboot/Reboot.kt
@@ -1,11 +1,29 @@
 package net.milosvasic.factory.mail.component.installer.step.reboot
 
 import net.milosvasic.factory.mail.component.installer.step.InstallationStep
+import net.milosvasic.factory.mail.component.packaging.PackageInstallerInitializationOperation
 import net.milosvasic.factory.mail.log
+import net.milosvasic.factory.mail.operation.Command
+import net.milosvasic.factory.mail.operation.OperationResult
+import net.milosvasic.factory.mail.operation.OperationResultListener
 import net.milosvasic.factory.mail.remote.Connection
+import net.milosvasic.factory.mail.remote.ssh.SSHCommand
 import net.milosvasic.factory.mail.terminal.Commands
 
 class Reboot(private val timeoutInSeconds: Int = 120) : InstallationStep<Connection>() {
+
+    private val command = Commands.reboot()
+
+    private val listener = object : OperationResultListener {
+        override fun onOperationPerformed(result: OperationResult) {
+
+            when (result.operation) {
+                is SSHCommand -> {
+
+                }
+            }
+        }
+    }
 
     @Synchronized
     @Throws(IllegalArgumentException::class)
@@ -17,7 +35,7 @@ class Reboot(private val timeoutInSeconds: Int = 120) : InstallationStep<Connect
         val connection = params[0]
 
         log.v("Reboot timeout in seconds: $timeoutInSeconds")
-        connection.execute(Commands.reboot())
+        connection.execute(command)
 
         // TODO: Execute and send RebootOperation
     }
