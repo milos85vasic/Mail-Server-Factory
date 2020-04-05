e562a66c90d6ca9fae57ad691a49da5f5cd8bc1c
Milos Vasic <milos85vasic@gmail.com>
2020-04-05 18:45:36 +0200

    MSF-65 - Working on reboot operation.

--- Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/reboot/Reboot.kt
+++ Jvm/Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/reboot/Reboot.kt
@@ -1,5 +1,8 @@
 package net.milosvasic.factory.mail.component.installer.step.reboot
 
+import net.milosvasic.factory.mail.common.busy.Busy
+import net.milosvasic.factory.mail.common.busy.BusyDelegation
+import net.milosvasic.factory.mail.common.busy.BusyWorker
 import net.milosvasic.factory.mail.component.installer.step.InstallationStep
 import net.milosvasic.factory.mail.component.packaging.PackageInstallerInitializationOperation
 import net.milosvasic.factory.mail.log
@@ -12,31 +15,46 @@ import net.milosvasic.factory.mail.terminal.Commands
 
 class Reboot(private val timeoutInSeconds: Int = 120) : InstallationStep<Connection>() {
 
+    private val busy = Busy()
     private val command = Commands.reboot()
+    private var connection: Connection? = null
 
     private val listener = object : OperationResultListener {
         override fun onOperationPerformed(result: OperationResult) {
 
             when (result.operation) {
                 is SSHCommand -> {
+                    if (result.operation.command.endsWith(command)) {
 
+                        connection?.unsubscribe(this)
+                        if (result.success) {
+
+                            // TODO: Ping until success.
+                        } else {
+
+                            // TODO: Notify on failure.
+                        }
+                    }
                 }
             }
         }
     }
 
     @Synchronized
-    @Throws(IllegalArgumentException::class)
+    @Throws(IllegalArgumentException::class, IllegalStateException::class)
     override fun execute(vararg params: Connection) {
 
         if (params.size > 1 || params.isEmpty()) {
             throw IllegalArgumentException("Expected 1 argument")
         }
-        val connection = params[0]
+        BusyWorker.busy(busy)
 
         log.v("Reboot timeout in seconds: $timeoutInSeconds")
-        connection.execute(command)
-
-        // TODO: Execute and send RebootOperation
+        connection = params[0]
+        if (connection == null) {
+            throw IllegalArgumentException("Connection is null.")
+        }
+        connection?.subscribe(listener)
+        connection?.execute(command)
     }
 }
