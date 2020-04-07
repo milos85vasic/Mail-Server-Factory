f63e7977103c10a8bc03e961a75683bcf24fe3a5
Milos Vasic <milos85vasic@gmail.com>
2020-04-07 16:03:27 +0200

    MSF-70 - Removing legacy code.

--- Legacy/Toolkit/README.md
+++
@@ -1,2 +0,0 @@
-# Factory Toolkit
-Contains common utilities needed to perform basic system commands.
--- Legacy/Toolkit/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-from .commands import *
-from .configuration import *
-from .connectivity import *
-from .mysql_common import *
-from .mysql_common_5560 import *
-from .system_configuration import *
