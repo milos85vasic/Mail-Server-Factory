e7fc8170b7dd87540ba4937d72fc3bcc98c74236
Milos Vasic <milos85vasic@gmail.com>
2020-04-07 16:43:35 +0200

    MSF-69 - Removed unused import.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/RemoteOperationInstallationStep.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/RemoteOperationInstallationStep.kt
@@ -4,7 +4,6 @@ import net.milosvasic.factory.mail.common.Notifying
 import net.milosvasic.factory.mail.common.Subscription
 import net.milosvasic.factory.mail.common.busy.Busy
 import net.milosvasic.factory.mail.common.busy.BusyWorker
-import net.milosvasic.factory.mail.component.installer.step.reboot.RebootOperation
 import net.milosvasic.factory.mail.operation.Operation
 import net.milosvasic.factory.mail.operation.OperationResult
 import net.milosvasic.factory.mail.operation.OperationResultListener
