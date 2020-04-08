8d630b6e5fe3c477b3967fb8af4e4065173130d8
Milos Vasic <milos85vasic@gmail.com>
2020-04-08 17:49:42 +0200

    MSF-72 - Some docker commands added.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/docker/DockerCommand.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/docker/DockerCommand.kt
@@ -2,5 +2,10 @@ package net.milosvasic.factory.mail.docker
 
 enum class DockerCommand(val command: String) {
 
-    RUN("run")
+    RUN("run"),
+    BUILD("build"),
+    COMMIT("commit"),
+    START("start"),
+    STOP("stop"),
+    KILL("kill")
 }
