ce9bb41bf2ab93799f6c73ef9f5b3f9a120d564f
Milos Vasic <milos85vasic@gmail.com>
2020-04-13 17:17:54 +0200

    MSF-76 - Copy operation - Lifecycle close.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/copy/Copy.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/copy/Copy.kt
@@ -1,23 +1,26 @@
 package net.milosvasic.factory.mail.component.installer.step.copy
 
 import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
+import net.milosvasic.factory.mail.log
+import net.milosvasic.factory.mail.operation.Command
 import net.milosvasic.factory.mail.operation.OperationResult
 import net.milosvasic.factory.mail.remote.ssh.SSH
 import net.milosvasic.factory.mail.remote.ssh.SSHCommand
+import net.milosvasic.factory.mail.terminal.Commands
 
 class Copy(what: String, where: String) : RemoteOperationInstallationStep<SSH>() {
 
     companion object {
 
-        val delimiter = ":"
+        const val delimiter = ":"
     }
 
     private var command = "" // TODO:
 
     override fun handleResult(result: OperationResult) {
         when (result.operation) {
-            is SSHCommand -> {
-                if (result.operation.command.endsWith(command)) {
+            is Command -> {
+                if (result.operation.toExecute.endsWith(command)) {
 
                     // TODO:
                     finish(result.success, CopyOperation())
@@ -30,6 +33,15 @@ class Copy(what: String, where: String) : RemoteOperationInstallationStep<SSH>()
     @Throws(IllegalArgumentException::class, IllegalStateException::class)
     override fun execute(vararg params: SSH) {
         super.execute(*params)
-        connection?.execute(command)
+        val terminal = connection?.terminal
+        if (terminal == null) {
+
+            log.e("No terminal for copying")
+            finish(false, CopyOperation())
+        } else {
+
+            command = "echo 'WORK IN PROGRESS'"
+            terminal.execute(Command(command))
+        }
     }
 }
