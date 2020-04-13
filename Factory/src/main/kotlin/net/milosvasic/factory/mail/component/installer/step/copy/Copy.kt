c68df8a6257568b8d46e96ca590452fa9c1b12b2
Milos Vasic <milos85vasic@gmail.com>
2020-04-13 16:54:33 +0200

    MSF-76 - Copy operation.

---
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/copy/Copy.kt
@@ -0,0 +1,31 @@
+package net.milosvasic.factory.mail.component.installer.step.copy
+
+import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
+import net.milosvasic.factory.mail.operation.OperationResult
+import net.milosvasic.factory.mail.remote.Connection
+import net.milosvasic.factory.mail.remote.ssh.SSH
+import net.milosvasic.factory.mail.remote.ssh.SSHCommand
+
+class Copy(what: String, where: String) : RemoteOperationInstallationStep<SSH>() {
+
+    private var command = "" // TODO:
+
+    override fun handleResult(result: OperationResult) {
+        when (result.operation) {
+            is SSHCommand -> {
+                if (result.operation.command.endsWith(command)) {
+
+                    // TODO:
+                    finish(result.success, CopyOperation())
+                }
+            }
+        }
+    }
+
+    @Synchronized
+    @Throws(IllegalArgumentException::class, IllegalStateException::class)
+    override fun execute(vararg params: SSH) {
+        super.execute(*params)
+        connection?.execute(command)
+    }
+}
---
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/copy/CopyOperation.kt
@@ -0,0 +1,5 @@
+package net.milosvasic.factory.mail.component.installer.step.copy
+
+import net.milosvasic.factory.mail.operation.Operation
+
+class CopyOperation : Operation()
