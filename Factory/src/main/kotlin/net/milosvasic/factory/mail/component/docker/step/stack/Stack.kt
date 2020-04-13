54df429f659795953d5c80f47c1fd399c8e6ef4a
Milos Vasic <milos85vasic@gmail.com>
2020-04-13 16:35:22 +0200

    MSF-74 - Deploy YML.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/docker/step/stack/Stack.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/docker/step/stack/Stack.kt
@@ -8,11 +8,13 @@ import net.milosvasic.factory.mail.configuration.ConfigurationManager
 import net.milosvasic.factory.mail.operation.Command
 import net.milosvasic.factory.mail.operation.OperationResult
 import net.milosvasic.factory.mail.remote.Connection
+import net.milosvasic.factory.mail.terminal.Commands
 import java.io.File
 
 
 class Stack(private val composeYmlPath: String) : DockerInstallationStep() {
 
+    private var ymlDeployed = false
     private var command = String.EMPTY
 
     override fun handleResult(result: OperationResult) {
@@ -21,7 +23,25 @@ class Stack(private val composeYmlPath: String) : DockerInstallationStep() {
             is Command -> {
                 if (command != String.EMPTY && result.operation.toExecute.endsWith(command)) {
 
-                    finish(result.success, DockerInstallationOperation())
+                    if (ymlDeployed) {
+                        finish(result.success, DockerInstallationOperation())
+                    } else {
+
+                        ymlDeployed = true
+                        var args = String.EMPTY
+                        val variables = ConfigurationManager.getConfiguration().variables
+                        if (variables.isEmpty()) {
+                            args = String.EMPTY
+                        } else {
+                            variables.keys.forEach { key ->
+                                args += " $key=${variables[key]}"
+                            }
+                        }
+                        val path = getYmlPath()
+                        val flags = "-d --remove-orphans"
+                        command = "$args ${DockerCommand.COMPOSE.command} -f $path ${DockerCommand.UP.command} $flags"
+                        connection?.execute(command)
+                    }
                 }
             }
         }
@@ -31,21 +51,25 @@ class Stack(private val composeYmlPath: String) : DockerInstallationStep() {
     @Throws(IllegalArgumentException::class, IllegalStateException::class)
     override fun execute(vararg params: Connection) {
         super.execute(*params)
-        var args = String.EMPTY
-        val variables = ConfigurationManager.getConfiguration().variables
-        if (variables.isEmpty()) {
-            args = String.EMPTY
-        } else {
-            variables.keys.forEach { key ->
-                args += " $key=${variables[key]}"
-            }
+        val remote = connection?.getRemote()
+        remote?.let {
+            command = Commands.scp(
+                    getYmlPath(),
+                    getYmlPath(),
+                    remote
+            )
         }
+        if (command == String.EMPTY) {
+            throw IllegalArgumentException("No remote available for the connection")
+        }
+        connection?.execute(command)
+    }
+
+    private fun getYmlPath(): String {
         var path = composeYmlPath
         if (!path.endsWith(".yml")) {
             path += File.separator + "docker-compose.yml"
         }
-        val flags = "-d --remove-orphans"
-        command = "$args ${DockerCommand.COMPOSE.command} -f $path ${DockerCommand.UP.command} $flags"
-        connection?.execute(command)
+        return path
     }
 }
