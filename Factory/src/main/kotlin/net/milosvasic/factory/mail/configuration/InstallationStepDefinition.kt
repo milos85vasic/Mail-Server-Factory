9709fe11e4c68bef3f61ef5edc2e4e93c28303de
Milos Vasic <milos85vasic@gmail.com>
2020-04-15 19:10:56 +0200

    MSF-79 - Refactoring parameters and paths.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/InstallationStepDefinition.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/InstallationStepDefinition.kt
@@ -1,7 +1,6 @@
 package net.milosvasic.factory.mail.configuration
 
 import net.milosvasic.factory.mail.EMPTY
-import java.util.regex.Pattern
 
 class InstallationStepDefinition(
     val type: String,
@@ -10,21 +9,5 @@ class InstallationStepDefinition(
 ) {
 
     @Throws(IllegalStateException::class)
-    fun getValue(): String {
-
-        val variableOpening = "{{"
-        val variableClosing = "}}"
-        val regex = "${Pattern.quote(variableOpening)}(.*?)${Pattern.quote(variableClosing)}"
-        val pattern = Pattern.compile(regex)
-        var result = value
-        val matcher = pattern.matcher(result)
-        while (matcher.find()) {
-            val match = matcher.group(1)
-            if (match.isNotEmpty()) {
-                val variable = ConfigurationManager.getConfiguration().variables[match]
-                result = result.replace("$variableOpening$match$variableClosing", variable.toString())
-            }
-        }
-        return result
-    }
+    fun getValue(): String = Variable.parse(value)
 }
---
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/Variable.kt
@@ -0,0 +1,30 @@
+package net.milosvasic.factory.mail.configuration
+
+import java.util.regex.Pattern
+
+object Variable {
+
+    private const val open = "{{"
+    private const val close = "}}"
+
+    private fun getRegex() = "${Pattern.quote(open)}(.*?)${Pattern.quote(close)}"
+
+    private fun getPattern() = Pattern.compile(getRegex())
+
+    @Throws(IllegalStateException::class)
+    fun parse(value: String): String {
+
+        val pattern = getPattern()
+        var result = value
+        val matcher = pattern.matcher(result)
+        while (matcher.find()) {
+            val match = matcher.group(1)
+            if (match.isNotEmpty()) {
+                val rawVariable = ConfigurationManager.getConfiguration().variables[match].toString()
+                val variable = parse(rawVariable)
+                result = result.replace("$open$match$close", variable)
+            }
+        }
+        return result
+    }
+}
