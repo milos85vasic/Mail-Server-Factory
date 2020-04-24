9f0e5055eae09af7f45740070eba1e52bb79e71b
Milos Vasic <milos85vasic@gmail.com>
2020-04-24 12:01:39 +0200

    MSF-82 - Cleanup.

--- Docker/Postfix/Scripts/start.sh
+++ Docker/Postfix/Scripts/start.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# adding IP of a host to /etc/hosts
-export HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
-echo "$HOST_IP dockerhost" >> /etc/hosts
-
-echo "Starting Postfix on `hostname` (IP: $HOST_IP)"
-
+echo "Starting Postfix on `hostname`"
 
 # TODO: Tbd.
 
-rsyslogd
 postfix start && postfix check
-tail -f /var/log/mail.log
