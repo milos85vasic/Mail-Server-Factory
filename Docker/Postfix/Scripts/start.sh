8d4d6be82d363042b5c6bf8db0076e7a8450cdf2
Milos Vasic <milos85vasic@gmail.com>
2020-04-24 11:49:26 +0200

    MSF-82 - Host IP.

--- Docker/Postfix/Scripts/start.sh
+++ Docker/Postfix/Scripts/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo "Starting Postfix on `hostname`"
+# adding IP of a host to /etc/hosts
+export HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+echo "$HOST_IP dockerhost" >> /etc/hosts
+
+echo "Starting Postfix on `hostname` (IP: $HOST_IP)"
+
 
 # TODO: Tbd.
 
