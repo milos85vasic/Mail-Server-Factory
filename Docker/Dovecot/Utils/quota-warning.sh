6fcabfd8e9c88c148338b038d3206ef3d2002250
Milos Vasic <milos85vasic@gmail.com>
2020-04-30 14:52:50 +0200

    MSF-123 - Quota warnings.

---
+++ Docker/Dovecot/Utils/quota-warning.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PERCENT=$1
+USER=$2
+
+cat <<EOF | /usr/lib/dovecot/dovecot-lda -d ${USER} -o "plugin/quota=maildir
+    From: postmaster@example.com
+    Subject: Quota warning
+    Your mailbox is currently ${PERCENT}% full.
+EOF
