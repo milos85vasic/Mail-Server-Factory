ab95e40a2880e59fa370b8e860b7ee7d56f8491f
Milos Vasic <milos85vasic@gmail.com>
2020-04-30 14:58:46 +0200

    MSF-123 - TODO.

--- Docker/Dovecot/Utils/proto.quota-warning.sh
+++ Docker/Dovecot/Utils/proto.quota-warning.sh
@@ -3,6 +3,7 @@
 PERCENT=$1
 USER=$2
 
+# TODO: Replace postmaster@example.com with  __PROPER_MAIL__
 cat <<EOF | /usr/lib/dovecot/dovecot-lda -d ${USER} -o plugin/quota=maildir
     From: postmaster@example.com
     Subject: Quota warning
