f5331dc59ff7eb198ddd3da54eec007567095d18
Milos Vasic <milos85vasic@gmail.com>
2020-05-06 16:31:03 +0200

    MSF-89 - Scoping the flow.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/execution/flow/CommandsFlow.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/execution/flow/CommandsFlow.kt
@@ -1,9 +1,34 @@
 package net.milosvasic.factory.mail.execution.flow
 
 import net.milosvasic.factory.mail.common.Executor
+import net.milosvasic.factory.mail.common.busy.BusyException
 import net.milosvasic.factory.mail.terminal.TerminalCommand
 
-class CommandsFlow : Flow<Executor<TerminalCommand>, TerminalCommand>() {
+class CommandsFlow : FlowBuilder<Executor<TerminalCommand>, TerminalCommand>() {
+
+    @Throws(BusyException::class)
+    override fun width(subject: Executor<TerminalCommand>): CommandsFlow {
+        super.width(subject)
+        return this
+    }
+
+    @Throws(BusyException::class)
+    fun perform(what: String): CommandsFlow {
+        perform(TerminalCommand(what))
+        return this
+    }
+
+    @Throws(BusyException::class)
+    override fun perform(what: TerminalCommand): CommandsFlow {
+        super.perform(what)
+        return this
+    }
+
+    @Throws(BusyException::class)
+    override fun onFinish(callback: FlowCallback): CommandsFlow {
+        super.onFinish(callback)
+        return this
+    }
 
     override fun getProcessingRecipe(
             subject: Executor<TerminalCommand>,
--- Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
@@ -3,6 +3,10 @@
 package net.milosvasic.factory.mail
 
 import net.milosvasic.factory.mail.application.ServerFactory
+import net.milosvasic.factory.mail.execution.flow.CommandsFlow
+import net.milosvasic.factory.mail.terminal.Commands
+import net.milosvasic.factory.mail.terminal.Terminal
+import net.milosvasic.factory.mail.terminal.TerminalCommand
 import net.milosvasic.logger.ConsoleLogger
 import net.milosvasic.logger.FilesystemLogger
 import java.io.File
@@ -10,6 +14,20 @@ import java.io.File
 fun main(args: Array<String>) {
 
     initLogging()
+
+
+
+    val terminal = Terminal()
+    CommandsFlow()
+            .width(terminal)
+            .perform(TerminalCommand(Commands.echo("Moja Maja")))
+            .perform("Moja Maja")
+            .run()
+
+    return // TODO: Remove when flow implementation is complete.
+
+
+
     ServerFactory().run(args)
 }
 
