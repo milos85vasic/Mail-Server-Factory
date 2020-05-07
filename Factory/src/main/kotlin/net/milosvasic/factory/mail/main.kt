10fbcc93db5587eda362bb89556e90e5a7260d62
Milos Vasic <milos85vasic@gmail.com>
2020-05-07 11:52:33 +0200

    MSF-89 - Cleaning up.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
@@ -3,56 +3,13 @@
 package net.milosvasic.factory.mail
 
 import net.milosvasic.factory.mail.application.ServerFactory
-import net.milosvasic.factory.mail.execution.flow.FlowCallback
-import net.milosvasic.factory.mail.execution.flow.command.CommandFlow
-import net.milosvasic.factory.mail.remote.Remote
-import net.milosvasic.factory.mail.remote.ssh.SSH
-import net.milosvasic.factory.mail.terminal.Commands
-import net.milosvasic.factory.mail.terminal.Terminal
-import net.milosvasic.factory.mail.terminal.TerminalCommand
 import net.milosvasic.logger.ConsoleLogger
 import net.milosvasic.logger.FilesystemLogger
 import java.io.File
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
 
     initLogging()
-
-
-
-    val flowCallback = object : FlowCallback{
-        override fun onFinish(success: Boolean, message: String) {
-            if (success) {
-                log.i("Flow finished")
-            } else {
-                log.e(message)
-            }
-            exitProcess(0)
-        }
-    }
-
-    var count = 1
-    val terminal = Terminal()
-    val ssh = SSH(Remote("192.168.100.101", 22, "root"))
-    CommandFlow()
-            .width(terminal)
-            .perform(TerminalCommand(Commands.echo("Moja Maja ${count++}")))
-            .perform(Commands.echo("Moja Maja ${count++}"))
-            .perform(TerminalCommand(Commands.echo("Moja Maja ${count++}")))
-            .perform(Commands.echo("Moja Maja ${count++}"))
-            .width(ssh)
-            .perform(TerminalCommand(Commands.echo("Moja Maja ${count++}")))
-            .perform(Commands.echo("Moja Maja ${count++}"))
-            .perform(TerminalCommand(Commands.echo("Moja Maja ${count++}")))
-            .perform(Commands.echo("Moja Maja ${count++}"))
-            .onFinish(flowCallback)
-            .run()
-
-    return // TODO: Remove when flow implementation is complete.
-
-
-
     ServerFactory().run(args)
 }
 
