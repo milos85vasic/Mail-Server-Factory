f370b9a1149150b2fa650a992268259cd90daca1
Milos Vasic <milos85vasic@gmail.com>
2020-05-08 17:56:28 +0200

    MSF-89 - Default init. handler.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/main.kt
@@ -2,39 +2,19 @@
 
 package net.milosvasic.factory.mail
 
+import net.milosvasic.factory.mail.application.DefaultInitializationHandler
 import net.milosvasic.factory.mail.application.server_factory.ServerFactory
-import net.milosvasic.factory.mail.application.server_factory.ServerFactoryTerminationOperation
 import net.milosvasic.factory.mail.error.ERROR
 import net.milosvasic.factory.mail.execution.flow.callback.FlowCallback
 import net.milosvasic.factory.mail.execution.flow.implementation.InitializationFlow
-import net.milosvasic.factory.mail.operation.OperationResult
-import net.milosvasic.factory.mail.operation.OperationResultListener
 import net.milosvasic.logger.ConsoleLogger
 import net.milosvasic.logger.FilesystemLogger
 import java.io.File
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
 
     initLogging()
     val factory = ServerFactory(args.toList())
-
-    val initializationCallback = object : OperationResultListener {
-        override fun onOperationPerformed(result: OperationResult) {
-            when (result.operation) {
-                is ServerFactoryTerminationOperation -> {
-                    if (result.success) {
-                        log.i("Server factory terminated")
-                        exitProcess(0)
-                    } else {
-                        fail(ERROR.TERMINATION_FAILURE)
-                    }
-                }
-            }
-        }
-    }
-
-    factory.subscribe(initializationCallback)
 
     val callback = object : FlowCallback<String> {
         override fun onFinish(success: Boolean, message: String, data: String?) {
@@ -48,14 +28,14 @@ fun main(args: Array<String>) {
                     log.e(e)
                     fail(ERROR.RUNTIME_ERROR)
                 }
-            } else {
-                fail(ERROR.INITIALIZATION_FAILURE)
             }
         }
     }
 
+    val handler = DefaultInitializationHandler()
+
     InitializationFlow()
-            .width(factory) // TODO: Terminate width parameter - passed like we did with DataHandler
+            .width(factory, handler)
             .onFinish(callback)
             .run()
 }
