6eebe94f8460d702c014267fbe57bf4c12abf41a
Milos Vasic <milos85vasic@gmail.com>
2020-05-08 17:01:01 +0200

    MSF-89 - Initialization handler - Writing test.

--- Factory/src/test/kotlin/net/milosvasic/factory/mail/test/InitializationFlowTest.kt
+++ Factory/src/test/kotlin/net/milosvasic/factory/mail/test/InitializationFlowTest.kt
@@ -3,8 +3,10 @@ package net.milosvasic.factory.mail.test
 import net.milosvasic.factory.mail.common.initialization.Initializer
 import net.milosvasic.factory.mail.execution.flow.callback.FlowCallback
 import net.milosvasic.factory.mail.execution.flow.implementation.InitializationFlow
+import net.milosvasic.factory.mail.execution.flow.implementation.InitializationHandler
 import net.milosvasic.factory.mail.log
 import net.milosvasic.factory.mail.test.implementation.SimpleInitializer
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class InitializationFlowTest : BaseTest() {
@@ -43,6 +45,61 @@ class InitializationFlowTest : BaseTest() {
         while (!finished) {
             Thread.yield()
         }
+        log.i("Test: COMPLETED")
+    }
+
+    @Test
+    fun testInitializationFlowWithHandler() {
+        initLogging()
+        log.i("Test: STARTED")
+
+        val count = 5
+        val initializers = mutableListOf<Initializer>()
+        for (x in 0 until count) {
+            val initializer = SimpleInitializer("Initializer no. ${x + 1}")
+            initializers.add(initializer)
+        }
+
+        var finished = false
+        val flowCallback = object : FlowCallback<String> {
+
+            override fun onFinish(success: Boolean, message: String, data: String?) {
+                if (!success) {
+                    log.e(message)
+                }
+                assert(success)
+                finished = true
+            }
+        }
+
+        var initialized = 0
+        var terminated = 0
+        val handler = object : InitializationHandler {
+            override fun onInitialization(success: Boolean) {
+                assert(success)
+                initialized++
+            }
+
+            override fun onTermination(success: Boolean) {
+                assert(success)
+                terminated++
+            }
+        }
+
+        var flow = InitializationFlow()
+        initializers.forEach {
+            flow = flow.width(it, handler)
+        }
+        flow
+                .onFinish(flowCallback)
+                .run()
+
+        while (!finished) {
+            Thread.yield()
+        }
+
+        Assertions.assertEquals(count, initialized)
+        Assertions.assertEquals(count, terminated)
         log.i("Test: COMPLETED")
     }
 }
