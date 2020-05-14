c1ce21f9f52b4e279eb1be58c76f7d76fd19343b
Milos Vasic <milos85vasic@gmail.com>
2020-05-14 15:20:37 +0200

    MSF-142 - Introducing positive condition.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/condition/SkipConditionOperation.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/condition/SkipConditionOperation.kt
@@ -2,4 +2,4 @@ package net.milosvasic.factory.mail.component.installer.step.condition
 
 import net.milosvasic.factory.mail.operation.Operation
 
-class SkipConditionOperation(val result: Boolean) : Operation()
+open class SkipConditionOperation(val result: Boolean) : Operation()
