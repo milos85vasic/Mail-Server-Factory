45379260e0ce03072f009fdd55efdc487d7fb366
Milos Vasic <milos85vasic@gmail.com>
2020-05-17 15:03:13 +0200

    MSF-143 - Check step test - First touches.

---
+++ Factory/src/test/kotlin/net/milosvasic/factory/mail/test/CheckStepTest.kt
@@ -0,0 +1,17 @@
+package net.milosvasic.factory.mail.test
+
+import net.milosvasic.factory.mail.log
+import org.junit.jupiter.api.Test
+
+class CheckStepTest : BaseTest() {
+
+    @Test
+    fun testCheckStep() {
+        initLogging()
+        log.i("Check step test started")
+
+
+
+        log.i("Check step test completed")
+    }
+}
