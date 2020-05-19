6e6fbb604a36ae3f6d5af5022d69a0ca4f0ccc38
Milos Vasic <milos85vasic@gmail.com>
2020-05-19 12:07:42 +0200

    MSF-140 - Stack test - Scoping the flow.

--- Factory/src/test/kotlin/net/milosvasic/factory/mail/test/StackStepTest.kt
+++ Factory/src/test/kotlin/net/milosvasic/factory/mail/test/StackStepTest.kt
@@ -1,6 +1,10 @@
 package net.milosvasic.factory.mail.test
 
+import net.milosvasic.factory.mail.application.server_factory.ServerFactory
+import net.milosvasic.factory.mail.execution.flow.callback.FlowCallback
+import net.milosvasic.factory.mail.fail
 import net.milosvasic.factory.mail.log
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class StackStepTest : BaseTest() {
@@ -10,7 +14,25 @@ class StackStepTest : BaseTest() {
         initLogging()
         log.i("Deploy step flow test started")
 
+        val mocks = "Mocks/Stack/Main.json"
+        val factory = ServerFactory(listOf(mocks))
 
+        val callback = object : FlowCallback<String> {
+            override fun onFinish(success: Boolean, message: String, data: String?) {
+
+                assert(success)
+                if (success) {
+                    try {
+                        log.i("Factory initialized")
+                        factory.run()
+                    } catch (e: IllegalStateException) {
+
+                        fail(e)
+                        Assertions.fail()
+                    }
+                }
+            }
+        }
 
         log.i("Deploy step flow test completed")
     }
