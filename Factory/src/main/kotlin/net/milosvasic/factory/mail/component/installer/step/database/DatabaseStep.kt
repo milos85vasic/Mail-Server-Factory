8fafadefacc8f4f454ec3fbbe61e2d99bb0b7d16
Milos Vasic <milos85vasic@gmail.com>
2020-05-25 14:21:29 +0200

    MSF-148 - Database registration.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/database/DatabaseStep.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/database/DatabaseStep.kt
@@ -1,8 +1,12 @@
 package net.milosvasic.factory.mail.component.installer.step.database
 
-import net.milosvasic.factory.mail.component.database.DatabaseInitializationOperation
+import net.milosvasic.factory.mail.component.database.*
 import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
+import net.milosvasic.factory.mail.execution.flow.implementation.RegistrationFlow
+import net.milosvasic.factory.mail.log
+import net.milosvasic.factory.mail.operation.OperationResult
+import net.milosvasic.factory.mail.operation.OperationResultListener
 import net.milosvasic.factory.mail.remote.ssh.SSH
 import net.milosvasic.factory.mail.terminal.command.TestCommand
 import java.io.File
@@ -19,10 +23,29 @@ class DatabaseStep(val path: String) : RemoteOperationInstallationStep<SSH>() {
 
         connection?.let { conn ->
 
+            val manager = DatabaseManager
             val configurationFile = "$path${File.separator}$defaultConfigurationFile"
+
+            // TODO: Instantiate registration
+            val registration = DatabaseRegistration(
+                    "Test", // TODO: Use from DB
+                    Postgres("Test", conn),
+                    object : OperationResultListener {
+                        override fun onOperationPerformed(result: OperationResult) {
+
+                            log.e("> > > > > > TODO: $result")
+                        }
+                    }
+            )
+
+            val registrationFlow = RegistrationFlow<DatabaseRegistration>()
+                    .width(manager)
+                    .perform(registration)
+
             return CommandFlow()
                     .width(conn)
                     .perform(TestCommand(configurationFile))
+                    .connect(registrationFlow)
         }
         throw IllegalArgumentException("No proper connection provided")
     }
---
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/execution/flow/implementation/RegistrationFlow.kt
@@ -0,0 +1,77 @@
+package net.milosvasic.factory.mail.execution.flow.implementation
+
+import net.milosvasic.factory.mail.common.Registration
+import net.milosvasic.factory.mail.common.busy.BusyException
+import net.milosvasic.factory.mail.execution.flow.FlowBuilder
+import net.milosvasic.factory.mail.execution.flow.FlowPerformBuilder
+import net.milosvasic.factory.mail.execution.flow.callback.FlowCallback
+import net.milosvasic.factory.mail.execution.flow.processing.FlowProcessingCallback
+import net.milosvasic.factory.mail.execution.flow.processing.ProcessingRecipe
+import net.milosvasic.factory.mail.getMessage
+
+class RegistrationFlow<T> : FlowPerformBuilder<Registration<T>, T, String>() {
+
+    @Throws(BusyException::class)
+    override fun width(subject: Registration<T>): RegistrationFlow<T> {
+        super.width(subject)
+        return this
+    }
+
+    @Throws(BusyException::class)
+    override fun perform(what: T): RegistrationFlow<T> {
+        super.perform(what)
+        return this
+    }
+
+    @Throws(BusyException::class)
+    override fun onFinish(callback: FlowCallback<String>): RegistrationFlow<T> {
+        super.onFinish(callback)
+        return this
+    }
+
+    @Throws(BusyException::class)
+    override fun connect(flow: FlowBuilder<*, *, *>): RegistrationFlow<T> {
+        super.connect(flow)
+        return this
+    }
+
+    @Throws(IllegalArgumentException::class)
+    override fun getProcessingRecipe(subject: Registration<T>, operation: T): ProcessingRecipe {
+
+        return object : ProcessingRecipe {
+
+            private var callback: FlowProcessingCallback? = null
+
+//            private val operationCallback = object : OperationResultListener {
+//                override fun onOperationPerformed(result: OperationResult) {
+//
+//                    subject.unsubscribe(this)
+//
+//                    val message = if (result.success) {
+//                        String.EMPTY
+//                    } else {
+//                        if (result.operation is TerminalCommand) {
+//                            "Registration failed: ${result.operation.command}"
+//                        } else {
+//                            "Registration failed: ${result.operation}"
+//                        }
+//                    }
+//                    callback?.onFinish(result.success, message)
+//                    callback = null
+//                }
+//            }
+
+            override fun process(callback: FlowProcessingCallback) {
+                this.callback = callback
+                // subject.subscribe(operationCallback)
+                try {
+                    subject.register(operation)
+                } catch (e: Exception) {
+
+                    // subject.unsubscribe(operationCallback)
+                    callback.onFinish(false, e.getMessage())
+                }
+            }
+        }
+    }
+}
