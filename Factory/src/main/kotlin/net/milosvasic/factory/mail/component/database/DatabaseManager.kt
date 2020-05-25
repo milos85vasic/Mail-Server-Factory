351a490abfa0f25ea8fd121ac617af3fdff8c132
Milos Vasic <milos85vasic@gmail.com>
2020-05-25 12:00:11 +0200

    MSF-148 - Database manager - Working on the implementation.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/database/DatabaseManager.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/database/DatabaseManager.kt
@@ -1,24 +1,52 @@
 package net.milosvasic.factory.mail.component.database
 
 import net.milosvasic.factory.mail.common.Registration
+import net.milosvasic.factory.mail.common.busy.Busy
+import net.milosvasic.factory.mail.common.busy.BusyException
+import net.milosvasic.factory.mail.common.busy.BusyWorker
 import net.milosvasic.factory.mail.common.obtain.ObtainParametrized
 
 object DatabaseManager :
         ObtainParametrized<Type, Database>,
-        Registration<Database>
-{
+        Registration<Database> {
 
+    private val busy = Busy()
     private val databases = mutableMapOf<Type, Database>()
 
+    @Synchronized
+    @Throws(IllegalStateException::class)
     override fun register(what: Database) {
-        TODO("Not yet implemented")
+        busy()
+
     }
 
+    @Throws(IllegalArgumentException::class)
     override fun unregister(what: Database) {
-        TODO("Not yet implemented")
+        val type = what.type
+        if (databases[type] == what) {
+            databases.remove(type)?.terminate()
+        } else {
+
+            throw IllegalArgumentException("Database instance is not register: $databases")
+        }
+    }
+
+    fun unregister(type: Type) {
+        databases.remove(type)?.terminate()
     }
 
     override fun obtain(vararg param: Type): Database {
         TODO("Not yet implemented")
+    }
+
+    @Synchronized
+    @Throws(BusyException::class)
+    private fun busy() {
+        BusyWorker.busy(busy)
+    }
+
+    @Synchronized
+    private fun free() {
+        BusyWorker.free(busy)
     }
 }
