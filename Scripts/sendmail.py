eb5789ee58bdd5046a98098fbfec937f073a1d7a
Milos Vasic <milos85vasic@gmail.com>
2020-05-28 12:19:39 +0200

    MSF-82 - Test script for sending email messages.

---
+++ Scripts/sendmail.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+
+port = 25
+smtp_server = "test1.dev.local"
+sender_email = "test1@dev.local"
+receiver_email = "test2@dev.local"
+password = "Test"
+message = "Test message"
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
