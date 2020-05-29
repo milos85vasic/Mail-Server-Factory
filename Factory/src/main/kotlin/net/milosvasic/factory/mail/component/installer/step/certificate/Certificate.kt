2434de941a4115c77d0786a7aa7c5db0c0868b0f
Milos Vasic <milos85vasic@gmail.com>
2020-05-29 13:46:33 +0200

    MSF-95 - Generating private key.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -2,16 +2,20 @@ package net.milosvasic.factory.mail.component.installer.step.certificate
 
 import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
-import net.milosvasic.factory.mail.operation.Operation
 import net.milosvasic.factory.mail.remote.ssh.SSH
 
 class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
 
     override fun getFlow(): CommandFlow {
-        TODO("Not yet implemented")
+
+        connection?.let { conn ->
+
+            return CommandFlow()
+                    .width(conn)
+                    .perform(GeneratePrivateKeyCommand(name))
+        }
+        throw IllegalArgumentException("No proper connection provided")
     }
 
-    override fun getOperation(): Operation {
-        TODO("Not yet implemented")
-    }
+    override fun getOperation() = CertificateInitializationOperation()
 }
---
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/GeneratePrivateKeyCommand.kt
@@ -0,0 +1,8 @@
+package net.milosvasic.factory.mail.component.installer.step.certificate
+
+import net.milosvasic.factory.mail.terminal.TerminalCommand
+
+class GeneratePrivateKeyCommand(name: String) : TerminalCommand(
+
+        ""
+)
