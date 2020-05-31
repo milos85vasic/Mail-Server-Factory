8d37a076c16014fc5af4419d12353f7f9323c66e
Milos Vasic <milos85vasic@gmail.com>
2020-05-31 16:04:49 +0200

    MSF-95 - Verify results.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -1,14 +1,18 @@
 package net.milosvasic.factory.mail.component.installer.step.certificate
 
 import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
+import net.milosvasic.factory.mail.component.installer.step.condition.SkipCondition
 import net.milosvasic.factory.mail.configuration.*
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
+import net.milosvasic.factory.mail.execution.flow.implementation.InstallationStepFlow
 import net.milosvasic.factory.mail.remote.ssh.SSH
 import net.milosvasic.factory.mail.security.Permission
 import net.milosvasic.factory.mail.security.Permissions
 import net.milosvasic.factory.mail.terminal.command.ChmodCommand
 import net.milosvasic.factory.mail.terminal.command.Commands
 import net.milosvasic.factory.mail.terminal.command.MkdirCommand
+import net.milosvasic.factory.mail.terminal.command.TestCommand
+import java.io.File
 
 class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
 
@@ -27,6 +31,10 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
             val path = configuration.getVariableParsed(key) as String
             val permission = Permissions(Permission.ALL, Permission.NONE, Permission.NONE)
             val perm = permission.obtain()
+            val sep = File.separator
+            val certificateExtension = ".crt"
+            val issued = "${sep}pki${sep}issued$sep"
+            val verificationPath = "{{SERVER.CERTIFICATION.HOME}}$issued$hostname$certificateExtension"
 
             return CommandFlow()
                     .width(conn)
@@ -36,6 +44,7 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
                     .perform(ImportRequestKeyCommand(path, Commands.getRequestKeyName(name), hostname))
                     .perform(SignRequestKeyCommand(hostname))
                     .perform(ChmodCommand(path, perm))
+                    .perform(TestCommand(Variable.parse(verificationPath)))
         }
         throw IllegalArgumentException("No proper connection provided")
     }
