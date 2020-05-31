82bf1bf4a1b4d08f3e4e7c28c829691dd94e9da9
Milos Vasic <milos85vasic@gmail.com>
2020-05-31 15:22:57 +0200

    MSF-95 - Setting up proper permissions.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -4,6 +4,9 @@ import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInsta
 import net.milosvasic.factory.mail.configuration.*
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
 import net.milosvasic.factory.mail.remote.ssh.SSH
+import net.milosvasic.factory.mail.security.Permission
+import net.milosvasic.factory.mail.security.Permissions
+import net.milosvasic.factory.mail.terminal.command.ChmodCommand
 import net.milosvasic.factory.mail.terminal.command.Commands
 import net.milosvasic.factory.mail.terminal.command.MkdirCommand
 
@@ -21,12 +24,15 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
             val key = "$ctxServer$ctxSeparator$ctxCertification$ctxSeparator$keyHome"
             val configuration = ConfigurationManager.getConfiguration()
             val path = configuration.getVariableParsed(key) as String
+            val permission = Permissions(Permission.ALL, Permission.NONE, Permission.NONE)
+            val perm = permission.obtain()
 
             return CommandFlow()
                     .width(conn)
                     .perform(MkdirCommand(path))
                     .perform(GeneratePrivateKeyCommand(path, name))
                     .perform(GenerateRequestKeyCommand(path, Commands.getPrivateKyName(name), name))
+                    .perform(ChmodCommand(path, perm))
         }
         throw IllegalArgumentException("No proper connection provided")
     }
