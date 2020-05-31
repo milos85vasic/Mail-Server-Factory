45f2333d41fdd71a0a0c3755b06b24ea05b65263
Milos Vasic <milos85vasic@gmail.com>
2020-05-31 16:40:19 +0200

    MSF-95 - Creating symbolic link.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -37,7 +37,7 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
             val issued = "${sep}pki${sep}issued$sep"
             val certHome = "{{SERVER.CERTIFICATION.HOME}}"
             val certificates = "{{SERVER.CERTIFICATION.CERTIFICATES}}"
-            val linkingPath = Variable.parse("$certificates$hostname$certificateExtension")
+            val linkingPath = Variable.parse("$certificates$sep$hostname$certificateExtension")
             val verificationPath = Variable.parse("$certHome$issued$hostname$certificateExtension")
             val verificationCommand = TestCommand(verificationPath)
 
