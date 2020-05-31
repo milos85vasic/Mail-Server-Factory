ff6576d1bcd44126c106dff067bf9307e14ad522
Milos Vasic <milos85vasic@gmail.com>
2020-05-31 14:45:51 +0200

    MSF-95 - Create certificates home directory.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -4,6 +4,7 @@ import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInsta
 import net.milosvasic.factory.mail.configuration.*
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
 import net.milosvasic.factory.mail.remote.ssh.SSH
+import net.milosvasic.factory.mail.terminal.command.MkdirCommand
 
 class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
 
@@ -22,6 +23,7 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
 
             return CommandFlow()
                     .width(conn)
+                    .perform(MkdirCommand(path))
                     .perform(GeneratePrivateKeyCommand(path, name))
         }
         throw IllegalArgumentException("No proper connection provided")
