9ab2fc3151ca6779894ff1e79421cf35b6ca9970
Milos Vasic <milos85vasic@gmail.com>
2020-05-31 16:26:11 +0200

    MSF-95 - Splitting the flow.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/component/installer/step/certificate/Certificate.kt
@@ -1,8 +1,14 @@
 package net.milosvasic.factory.mail.component.installer.step.certificate
 
+import net.milosvasic.factory.mail.component.Toolkit
+import net.milosvasic.factory.mail.component.installer.recipe.CommandInstallationStepRecipe
+import net.milosvasic.factory.mail.component.installer.recipe.ConditionRecipe
+import net.milosvasic.factory.mail.component.installer.step.CommandInstallationStep
 import net.milosvasic.factory.mail.component.installer.step.RemoteOperationInstallationStep
+import net.milosvasic.factory.mail.component.installer.step.condition.SkipCondition
 import net.milosvasic.factory.mail.configuration.*
 import net.milosvasic.factory.mail.execution.flow.implementation.CommandFlow
+import net.milosvasic.factory.mail.execution.flow.implementation.InstallationStepFlow
 import net.milosvasic.factory.mail.remote.ssh.SSH
 import net.milosvasic.factory.mail.security.Permission
 import net.milosvasic.factory.mail.security.Permissions
@@ -33,16 +39,28 @@ class Certificate(val name: String) : RemoteOperationInstallationStep<SSH>() {
             val certificateExtension = ".crt"
             val issued = "${sep}pki${sep}issued$sep"
             val verificationPath = "{{SERVER.CERTIFICATION.HOME}}$issued$hostname$certificateExtension"
+            val verificationCommand = TestCommand(Variable.parse(verificationPath))
+
+            val toolkit = Toolkit(conn)
+            val checkFlow = InstallationStepFlow(toolkit)
+                    .registerRecipe(SkipCondition::class, ConditionRecipe::class)
+                    .registerRecipe(CommandInstallationStep::class, CommandInstallationStepRecipe::class)
+                    .width(SkipCondition(verificationCommand))
+                    .width(CommandInstallationStep(GeneratePrivateKeyCommand(path, name)))
+                    .width(CommandInstallationStep(GenerateRequestKeyCommand(path, Commands.getPrivateKyName(name), name)))
+                    .width(CommandInstallationStep(ImportRequestKeyCommand(path, Commands.getRequestKeyName(name), hostname)))
+                    .width(CommandInstallationStep(SignRequestKeyCommand(hostname)))
+                    .width(CommandInstallationStep(ChmodCommand(path, perm)))
+
+            val completionFlow = CommandFlow()
+                    .width(conn)
+                    .perform(verificationCommand)
 
             return CommandFlow()
                     .width(conn)
                     .perform(MkdirCommand(path))
-                    .perform(GeneratePrivateKeyCommand(path, name))
-                    .perform(GenerateRequestKeyCommand(path, Commands.getPrivateKyName(name), name))
-                    .perform(ImportRequestKeyCommand(path, Commands.getRequestKeyName(name), hostname))
-                    .perform(SignRequestKeyCommand(hostname))
-                    .perform(ChmodCommand(path, perm))
-                    .perform(TestCommand(Variable.parse(verificationPath)))
+                    .connect(checkFlow)
+                    .connect(completionFlow)
         }
         throw IllegalArgumentException("No proper connection provided")
     }
