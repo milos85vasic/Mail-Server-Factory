2cdda2cc0946802dd1958ca9040bbc7b0a5f4d57
Milos Vasic <milos85vasic@gmail.com>
2020-06-01 13:33:00 +0200

    MSF-98 - Use main.cf.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-echo "Starting Postfix on `hostname`" > /var/log/postfix.log
-
-postfix start && postfix check > /var/log/postfix.log
+echo "Starting Postfix on `hostname`" >> /var/log/postfix.log
+#echo "- - - - - - - - - - - - - - - " >> /var/log/postfix.log
+#echo "Postfix configuration files:" >> /var/log/postfix.log
+#ls /etc/postfix >> /var/log/postfix.log
+#echo "- - - - - - - - - - - - - - - " >> /var/log/postfix.log
+#echo "Postfix configuration:" >> /var/log/postfix.log
+#cat /etc/postfix/main.cf >> /var/log/postfix.log
+#echo "- - - - - - - - - - - - - - - " >> /var/log/postfix.log
+#echo "Postfix certificates:" >> /var/log/postfix.log
+#ls /certificates >> /var/log/postfix.log
+#echo "- - - - - - - - - - - - - - - " >> /var/log/postfix.log
+postfix start && postfix check #>> /var/log/postfix.log
+#echo "Postfix started on `hostname`" >> /var/log/postfix.log
+#echo "- - - - - - - - - - - - - - - " >> /var/log/postfix.log
 tail -f /var/log/postfix.log
