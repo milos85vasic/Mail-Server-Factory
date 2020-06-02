b5a3f0551dccb89c849dc906b11eb3413f41a038
Milos Vasic <milos85vasic@gmail.com>
2020-06-02 15:45:41 +0200

    MSF-98 - Tail on success.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 echo "Starting Postfix on `hostname`" > /var/log/postfix.start.log
-# newaliases
 postfix set-permissions >> /var/log/postfix.start.log
 postfix check >> /var/log/postfix.start.log
 postfix start >> /var/log/postfix.start.log
-postfix status >> /var/log/postfix.start.log
-tail -f /var/log/postfix.start.log
+postfix status >> /var/log/postfix.start.log && tail -f /var/log/postfix.start.log
