d106c8a0f8e566e1981741fd676999a13b674e0d
Milos Vasic <milos85vasic@gmail.com>
2020-06-03 16:12:43 +0200

    MSF-103 - Check db port binding.

--- Definitions/Docker/Postfix/Scripts/proto.start.sh
+++ Definitions/Docker/Postfix/Scripts/proto.start.sh
@@ -4,6 +4,7 @@ echo "Starting Postfix on `hostname`" > /var/log/postfix.start.log
 echo "Checking database port: {{DB.DB_PORT}}" >> /var/log/postfix.start.log
 if ss -tulpn | grep ":{{DB.DB_PORT}}"
 then
+    echo "Process is available at port: {{DB.DB_PORT}}" >> /var/log/postfix.start.log
     postfix set-permissions >> /var/log/postfix.start.log
     postfix check >> /var/log/postfix.start.log
     postfix start >> /var/log/postfix.start.log
