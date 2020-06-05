fc16e3cebab3f739039046f80441aed009c9ad3a
Milos Vasic <milos85vasic@gmail.com>
2020-06-05 14:02:42 +0200

    MSF-157 - Verify Postfix port bindings.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -6,6 +6,23 @@ echo "Checking database port: $SERVICE_PORT" >> /var/log/postfix.start.log
 if echo "^C" | telnet postgres_database ${SERVICE_PORT} | grep "Connected"
 then
     echo "Process is available at port: $SERVICE_PORT" >> /var/log/postfix.start.log
+
+    if echo "^C" | telnet 127.0.0.1 465 | grep "Connected"
+    then
+        echo "Postfix is listening ar port: 465" >> /var/log/postfix.start.log
+    else
+        echo "Postfix is not bound to port: 465"
+        exit 1
+    fi
+
+    if echo "^C" | telnet 127.0.0.1 587 | grep "Connected"
+    then
+        echo "Postfix is listening ar port: 587" >> /var/log/postfix.start.log
+    else
+        echo "Postfix is not bound to port: 587"
+        exit 1
+    fi
+
     postfix set-permissions >> /var/log/postfix.start.log
     postfix check >> /var/log/postfix.start.log
     postfix start >> /var/log/postfix.start.log
