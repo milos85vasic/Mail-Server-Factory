4eea04c5338ea2344c22a746872a47b5b840eaa2
Milos Vasic <milos85vasic@gmail.com>
2020-06-05 16:07:04 +0200

    MSF-157 - Script polishing.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 
 dbPort=5432
-echo "Starting Postfix on `hostname`" > /var/log/postfix.start.log
-echo "Checking database port: $dbPort" >> /var/log/postfix.start.log
+postfixLog=/var/log/postfix.start.log
+echo "Starting Postfix on `hostname`" > ${postfixLog}
+echo "Checking database port: $dbPort" >> ${postfixLog}
 if echo "^C" | telnet postgres_database ${dbPort} | grep "Connected"
 then
-    echo "Process is available at port: $dbPort" >> /var/log/postfix.start.log
+    echo "Process is available on port: $dbPort" >> ${postfixLog}
 
-    postfix set-permissions >> /var/log/postfix.start.log
-    postfix check >> /var/log/postfix.start.log
-    postfix start >> /var/log/postfix.start.log
-    if postfix status >> /var/log/postfix.start.log
+    postfix set-permissions >> ${postfixLog}
+    postfix check >> ${postfixLog}
+    postfix start >> ${postfixLog}
+    if postfix status >> ${postfixLog}
     then
 
         ports=(465 587)
         for port in ${ports[@]}; do
             if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
             then
-                echo "Postfix is listening at port: $port" >> /var/log/postfix.start.log
+                echo "Postfix is listening on port: $port" >> ${postfixLog}
             else
-                echo "Postfix is not bound to port: $port"
+                echo "Postfix is not bound to port: $port" >> ${postfixLog}
                 exit 1
             fi
         done
 
-        tail -f /var/log/postfix.start.log
+        tail -f ${postfixLog}
     else
         exit 1
     fi
 else
-   echo "No process bound to port: $dbPort"
+   echo "No process bound to port: $dbPort" >> ${postfixLog}
    exit 1
 fi
