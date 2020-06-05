7f4bb5b1dbdab5787b2d45873a9c7e731d7dfe5b
Milos Vasic <milos85vasic@gmail.com>
2020-06-05 14:59:54 +0200

    MSF-157 - Polishing start script.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -7,23 +7,23 @@ if echo "^C" | telnet postgres_database ${dbPort} | grep "Connected"
 then
     echo "Process is available at port: $dbPort" >> /var/log/postfix.start.log
 
-    ports=(465 587)
-    for port in "${ports}"
-    do
-        if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
-        then
-            echo "Postfix is listening at port: $port" >> /var/log/postfix.start.log
-        else
-            echo "Postfix is not bound to port: $port"
-            exit 1
-        fi
-    done
-
     postfix set-permissions >> /var/log/postfix.start.log
     postfix check >> /var/log/postfix.start.log
     postfix start >> /var/log/postfix.start.log
     if postfix status >> /var/log/postfix.start.log
     then
+
+        ports=(465 587)
+        for port in ${ports[@]}; do
+            if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
+            then
+                echo "Postfix is listening at port: $port" >> /var/log/postfix.start.log
+            else
+                echo "Postfix is not bound to port: $port"
+                exit 1
+            fi
+        done
+
         tail -f /var/log/postfix.start.log
     else
         exit 1
