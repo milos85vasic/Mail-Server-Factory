cd4565b4aeeaf290fc253d9f325ce1dd3db7a7ac
Milos Vasic <milos85vasic@gmail.com>
2020-06-07 18:04:48 +0200

    MSF-157 - Script polishing.

--- Scripts/sendmail.py
+++ Scripts/sendmail.py
@@ -1,6 +1,6 @@
 import smtplib, ssl
 
-port = 465
+port = 587
 smtp_server = "test1.dev.local"
 sender_email = "test1@dev.local"
 receiver_email = "test2@dev.local"
@@ -10,7 +10,7 @@ message = "Test message"
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()
-    server.starttls(context=context)
-    server.ehlo()
+#     server.starttls(context=context)
+#     server.ehlo()
 #     server.login(sender_email, password)
 #     server.sendmail(sender_email, receiver_email, message)
