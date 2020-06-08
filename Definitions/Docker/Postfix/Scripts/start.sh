4de97a82361f6d77c51743d61026e785e003a7b8
Milos Vasic <milos85vasic@gmail.com>
2020-06-08 15:02:56 +0200

    MSF-105 - Check SASL port availability from Postfix side.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -1,35 +1,46 @@
 #!/bin/sh
 
 dbPort=5432
+dovecotSaslPort=12345
 postfixLog=/var/log/postfix.start.log
 echo "Starting Postfix on `hostname`" > ${postfixLog}
+
 echo "Checking database port: $dbPort" >> ${postfixLog}
 if echo "^C" | telnet postgres_database ${dbPort} | grep "Connected"
 then
-    echo "Process is available on port: $dbPort" >> ${postfixLog}
-
-    postfix set-permissions >> ${postfixLog}
-    postfix check >> ${postfixLog}
-    postfix start >> ${postfixLog}
-    if postfix status >> ${postfixLog}
-    then
-
-        ports=(465 587)
-        for port in ${ports[@]}; do
-            if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
-            then
-                echo "Postfix is listening on port: $port" >> ${postfixLog}
-            else
-                echo "Postfix is not bound to port: $port" >> ${postfixLog}
-                exit 1
-            fi
-        done
-
-        tail -f ${postfixLog}
-    else
-        exit 1
-    fi
+    echo "Database process is bound to port: $dbPort" >> ${postfixLog}
 else
    echo "No process bound to port: $dbPort" >> ${postfixLog}
    exit 1
+fi
+
+echo "Checking Dovecot SASL port: $dovecotSaslPort" >> ${postfixLog}
+if echo "^C" | telnet dovecot_service ${dovecotSaslPort} | grep "Connected"
+then
+    echo "Dovecot process is bound to port: $dovecotSaslPort" >> ${postfixLog}
+else
+   echo "No process bound to port: $dovecotSaslPort" >> ${postfixLog}
+   exit 1
+fi
+
+postfix set-permissions >> ${postfixLog}
+postfix check >> ${postfixLog}
+postfix start >> ${postfixLog}
+if postfix status >> ${postfixLog}
+then
+
+    ports=(465 587)
+    for port in ${ports[@]}; do
+        if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
+        then
+            echo "Postfix is listening on port: $port" >> ${postfixLog}
+        else
+            echo "Postfix is not bound to port: $port" >> ${postfixLog}
+            exit 1
+        fi
+    done
+
+    tail -f ${postfixLog}
+else
+    exit 1
 fi
