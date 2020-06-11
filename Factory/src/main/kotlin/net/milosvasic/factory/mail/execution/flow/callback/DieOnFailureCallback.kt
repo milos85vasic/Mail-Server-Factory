473d4191df19254053ed421409a13ea53de555ed
Milos Vasic <milos85vasic@gmail.com>
2020-06-11 17:51:28 +0200

    MSF-166 - Optimize imports.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/execution/flow/callback/DieOnFailureCallback.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/execution/flow/callback/DieOnFailureCallback.kt
@@ -3,7 +3,6 @@ package net.milosvasic.factory.mail.execution.flow.callback
 import net.milosvasic.factory.mail.error.ERROR
 import net.milosvasic.factory.mail.fail
 import net.milosvasic.factory.mail.log
-import java.lang.Exception
 
 open class DieOnFailureCallback : FlowCallback {
 
