1c30993a7811d4718ba384a2e9979fdda098b420
Milos Vasic <milos85vasic@gmail.com>
2020-06-19 15:56:37 +0200

    MSF-163 - Stripping POP ports.

--- Definitions/Docker/Postfix/Scripts/start.sh
+++ Definitions/Docker/Postfix/Scripts/start.sh
@@ -40,7 +40,7 @@ postfix start >> ${postfixLog}
 if postfix status >> ${postfixLog}
 then
 
-    ports=(465)
+    ports=(465 587)
     for port in ${ports[@]}; do
         if echo "^C" | telnet 127.0.0.1 ${port} | grep "Connected"
         then
