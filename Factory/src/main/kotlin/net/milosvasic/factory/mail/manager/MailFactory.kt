9a02ed703981f1a496352f9fe4d2fbef671b5da3
Milos Vasic <milos85vasic@gmail.com>
2020-06-23 14:43:02 +0200

    MSF-167 - Termination flow.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/manager/MailFactory.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/manager/MailFactory.kt
@@ -1,12 +1,16 @@
 package net.milosvasic.factory.mail.manager
 
 import net.milosvasic.factory.component.Toolkit
+import net.milosvasic.factory.execution.flow.callback.DieOnFailureCallback
 import net.milosvasic.factory.execution.flow.implementation.InstallationStepFlow
 
 class MailFactory(private val toolkit: Toolkit) {
 
     fun getMailCreationFlow(): InstallationStepFlow {
 
+        val dieCallback = DieOnFailureCallback()
         return InstallationStepFlow(toolkit)
+
+                .onFinish(dieCallback)
     }
 }
