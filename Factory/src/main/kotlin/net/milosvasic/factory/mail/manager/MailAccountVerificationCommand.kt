344932a190ab404f433b00f675941aa551aca303
Milos Vasic <milos85vasic@gmail.com>
2020-07-02 15:31:07 +0200

    MSF-167 - Verify with credentials.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/manager/MailAccountVerificationCommand.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/manager/MailAccountVerificationCommand.kt
@@ -2,8 +2,9 @@ package net.milosvasic.factory.mail.manager
 
 import net.milosvasic.factory.mail.account.MailAccount
 import net.milosvasic.factory.terminal.TerminalCommand
+import net.milosvasic.factory.terminal.command.Commands
 
 class MailAccountVerificationCommand(account: MailAccount) : TerminalCommand(
 
-        "doveadm auth lookup ${account.name} | grep \"${account.name}\""
+"${Commands.echo(account.getCredentials().value)} | doveadm auth test ${account.name}"
 )
