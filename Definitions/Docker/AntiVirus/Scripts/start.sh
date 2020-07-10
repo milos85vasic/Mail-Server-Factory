95f988b9e9b48da571eb9597682e1d4ca325524e
Milos Vasic <milos85vasic@gmail.com>
2020-07-10 14:55:03 +0200

    MSF-99 - Working on ClamAV service.

--- Definitions/Docker/AntiVirus/Scripts/start.sh
+++ Definitions/Docker/AntiVirus/Scripts/start.sh
@@ -6,7 +6,6 @@ chown clamscan /var/log/clamd.scan
 chgrp clamscan /etc/clamd.d/scan.conf
 
 sh /do_clam.sh
-# sleep 43200; sh /do_clam.sh &
-sleep 120; sh /do_clam.sh &
+sleep 43200; sh /do_clam.sh &
 
 tail -f /var/log/clamd.scan
