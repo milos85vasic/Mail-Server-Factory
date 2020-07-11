2e77a50fa0d575d7b67d69ec7953512ab7bba7cb
Milos Vasic <milos85vasic@gmail.com>
2020-07-11 13:53:04 +0200

    MSF-99 - Polishing logs.

--- Definitions/Docker/AntiVirus/Scripts/do_clam.sh
+++ Definitions/Docker/AntiVirus/Scripts/do_clam.sh
@@ -4,8 +4,7 @@ chgrp -R clamupdate /var/lib/clamav
 chown -R clamupdate /var/lib/clamav
 chmod 750 /var/lib/clamav
 
-clamLog=/var/log/clamd.run.log
-echo "===== ClamAV iteration: `date` ==========" > ${clamLog}
+clamLog=$1
 freshclam >> ${clamLog}
 clamscan --infected --remove --recursive /target >> ${clamLog}
 
--- Definitions/Docker/AntiVirus/Scripts/start.sh
+++ Definitions/Docker/AntiVirus/Scripts/start.sh
@@ -5,6 +5,8 @@ chgrp clamscan /var/log/clamd.scan
 chown clamscan /var/log/clamd.scan
 chgrp clamscan /etc/clamd.d/scan.conf
 
-sh /do_clam.sh
+clamLog=/var/log/clamd.run.log
+echo "ClamAV starting: `date`" > ${clamLog}
+sh /do_clam.sh ${clamLog}
 
-tail -F /var/log/clamd.run.log
+tail -F ${clamLog}
