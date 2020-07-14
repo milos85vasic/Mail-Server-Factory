e39b47c0fe60c6a5570aa1d0c9b7f927e4b2ad49
Milos Vasic <milos85vasic@gmail.com>
2020-07-14 16:08:43 +0200

    MSF-99 - Polishing log rotation.

--- Definitions/Docker/AntiVirus/Scripts/logrotate.sh
+++ Definitions/Docker/AntiVirus/Scripts/logrotate.sh
@@ -7,8 +7,7 @@ amavisLog="${logs}/${logFile}"
 cp ${amavisLog} "${logs}/$(($(date +%s%N)/1000000))_$logFile"
 echo "Log initialized: `date`" > ${amavisLog}
 
-find ${logs} -name "*_$logFile" -mmin +5 -exec rm -f {} \; >> ${amavisLog}
+find ${logs} -name "*_$logFile" -mtime +120 -exec rm -f {} \; >> ${amavisLog}
 echo "Beginning of log file:" >> ${amavisLog}
 
-sleep 60; sh /logrotate.sh &
-# sleep 604800; sh /logrotate.sh &
+sleep 604800; sh /logrotate.sh &
--- Definitions/Docker/Postfix/Scripts/logrotate.sh
+++ Definitions/Docker/Postfix/Scripts/logrotate.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 postfix logrotate
-# find /var/log/postfix -mtime +120 -exec rm -f {} \;
-find /var/log/postfix -mmin +5 -exec rm -f {} \;
+find /var/log/postfix -mtime +120 -exec rm -f {} \;
 
-# sleep 604800; sh /logrotate.sh &
-sleep 60; sh /logrotate.sh &
+sleep 604800; sh /logrotate.sh &
+
