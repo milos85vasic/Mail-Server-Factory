75df25bbb82abc6e8b3715815b126af0749b40ea
Milos Vasic <milos85vasic@gmail.com>
2020-08-07 17:11:42 +0200

    MSF-221 - Start Rspamd.

--- Definitions/Docker/AntiSpam/Scripts/proto.start.sh
+++ Definitions/Docker/AntiSpam/Scripts/proto.start.sh
@@ -4,4 +4,5 @@ logFile="rspamd.start.log"
 echo "Rspamd START: $(date)" > ${logFile}
 workerController=/etc/rspamd/local.d/worker-controller.inc
 echo "password = \"${rspamadm pw --encrypt -p {{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}}\"" > ${workerController}
+rspamd -u _rspamd -g _rspamd
 tail -F ${logFile}
