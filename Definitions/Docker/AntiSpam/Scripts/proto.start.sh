08c49dc17346be9bdbb9235c42483df0c08899a7
Milos Vasic <milos85vasic@gmail.com>
2020-08-07 17:27:32 +0200

    MSF-221 - FIXME added.

--- Definitions/Docker/AntiSpam/Scripts/proto.start.sh
+++ Definitions/Docker/AntiSpam/Scripts/proto.start.sh
@@ -3,6 +3,9 @@
 logFile="rspamd.start.log"
 echo "Rspamd START: $(date)" > ${logFile}
 workerController=/etc/rspamd/local.d/worker-controller.inc
-echo "password = \"${rspamadm pw --encrypt -p {{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}}\"" > ${workerController}
+# FIXME: Variable merging issue!
+# webuiPassword={{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}
+webuiPassword=Test12345
+echo "password = \"$(rspamadm pw --encrypt -p $webuiPassword)\"" > ${workerController}
 rspamd -u _rspamd -g _rspamd
 tail -F ${logFile}
