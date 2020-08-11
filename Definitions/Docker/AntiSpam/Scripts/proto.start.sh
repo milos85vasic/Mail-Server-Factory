b4be50e8be2851be40506159f20d793ae01ecb3e
Milos Vasic <milos85vasic@gmail.com>
2020-08-11 14:59:52 +0200

    MSF-226 - Using variable, reverted.

--- Definitions/Docker/AntiSpam/Scripts/proto.start.sh
+++ Definitions/Docker/AntiSpam/Scripts/proto.start.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 logFile="rspamd.start.log"
-echo "Rspamd START: $(date)" > ${logFile}
+echo "Rspamd START: $(date)" >${logFile}
 workerController=/etc/rspamd/local.d/worker-controller.inc
-# FIXME: Variable merging issue!
-# webuiPassword=__SERVICE.ANTI_SPAM.WEBUI.PASSWORD__
-webuiPassword=Test12345
-echo "password = \"$(rspamadm pw --encrypt -p $webuiPassword)\"" > ${workerController}
+webuiPassword={{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}
+echo "password = \"$(rspamadm pw --encrypt -p $webuiPassword)\"" >${workerController}
 rspamd -u _rspamd -g _rspamd
 tail -F ${logFile}
