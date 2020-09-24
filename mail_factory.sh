58b397a2e7dbdeb0cab2270ccf447fc72b8258e4
Milos Vasic <milos85vasic@gmail.com>
2020-09-25 01:09:24 +0200

    MSF-253 - Polishing.

--- mail_factory.sh
+++ mail_factory.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 script="factory_mail.sh"
-if test -e "$script"; then
+factoryPath="/usr/local/bin"
 
-  $script "mail" "$1"
+if test -e "$factoryPath/$script"; then
+
+  "$factoryPath/$script" "mail" "$1"
 else
 
-  echo "No $script found at: $(pwd)"
+  echo "No $script found at: $factoryPath"
   exit 1
 fi
