2bed8f23263697b8d0ab4f1b1cb35952b3364c73
Milos Vasic <milos85vasic@gmail.com>
2020-09-26 18:54:16 +0200

    MSF-215 - Installer: last touches.

--- factory_installer.sh
+++ factory_installer.sh
@@ -19,6 +19,7 @@ fi
 if test Application/Release/Application.jar; then
 
   factoryPath="/usr/local/bin"
+  echo "Please enter your sudo password to install the software" && \
   sudo cp -f Application/Release/Application.jar "$factoryPath/factory_$factoryType.jar" &&
     cp -f factory.sh "$factoryPath/factory_$factoryType.sh"
 else
