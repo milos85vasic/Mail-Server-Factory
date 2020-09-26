af63d690da49f78bac237c3f98f60d2d68c59109
Milos Vasic <milos85vasic@gmail.com>
2020-09-26 19:41:48 +0200

    MSF-215 - Installation result message.

--- factory_installer.sh
+++ factory_installer.sh
@@ -19,9 +19,16 @@ fi
 if test Application/Release/Application.jar; then
 
   factoryPath="/usr/local/bin"
-  echo "Please enter your sudo password to install the software" && \
-  sudo cp -f Application/Release/Application.jar "$factoryPath/factory_$factoryType.jar" &&
-    cp -f factory.sh "$factoryPath/factory_$factoryType.sh"
+  echo "Please enter your sudo password to install the software" &&
+    if sudo cp -f Application/Release/Application.jar "$factoryPath/factory_$factoryType.jar" &&
+      cp -f factory.sh "$factoryPath/factory_$factoryType.sh"; then
+
+      echo "Software has been installed with success"
+    else
+
+      echo "Software installation failed"
+      exit 1
+    fi
 else
 
   echo "No Application.jar found"
