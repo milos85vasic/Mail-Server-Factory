c053f8a92f93401033cb740afd66d208be90dd18
Milos Vasic <milos85vasic@gmail.com>
2020-09-27 23:16:55 +0200

    MSF-19 - Killing hard links.

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-installerScript=factory_installer.sh
+installerScript=Core/Utils/factory_installer.sh
 if test -e "$installerScript"; then
 
-  if ./$installerScript "mail"; then
+  if "$installerScript" "mail"; then
 
     factoryPath="/usr/local/bin"
-    sudo cp -f factory.sh "$factoryPath" &&
+    sudo cp -f Core/Utils/factory.sh "$factoryPath" &&
       cp -f mail_factory.sh "$factoryPath"
   else
 
