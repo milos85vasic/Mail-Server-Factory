39e5b337f8ccfb5e40ed7057f5b541f2fa643ae5
Milos Vasic <milos85vasic@gmail.com>
2020-10-05 14:11:59 +0200

    MSF-260 - Uses collection incorporated.

--- Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/MailServerConfiguration.kt
+++ Factory/src/main/kotlin/net/milosvasic/factory/mail/configuration/MailServerConfiguration.kt
@@ -3,6 +3,7 @@ package net.milosvasic.factory.mail.configuration
 import net.milosvasic.factory.EMPTY
 import net.milosvasic.factory.configuration.Configuration
 import net.milosvasic.factory.configuration.SoftwareConfiguration
+import net.milosvasic.factory.configuration.definition.Definition
 import net.milosvasic.factory.configuration.variable.Node
 import net.milosvasic.factory.mail.account.MailAccount
 import net.milosvasic.factory.remote.Remote
@@ -12,6 +13,7 @@ class MailServerConfiguration(
 
         name: String = String.EMPTY,
         remote: Remote,
+        uses: LinkedBlockingQueue<Definition>?,
         includes: LinkedBlockingQueue<String>?,
         software: LinkedBlockingQueue<String>?,
         containers: LinkedBlockingQueue<String>?,
@@ -22,7 +24,7 @@ class MailServerConfiguration(
 
 ) : Configuration(
 
-        name, remote, includes, software, containers, variables, overrides, enabled
+        name, remote, uses, includes, software, containers, variables, overrides, enabled
 ) {
 
     @Throws(IllegalArgumentException::class)
