c3fc73a013c0e8ec9b571d8247db3b69cd502564
Milos Vasic <milos85vasic@gmail.com>
2020-10-13 16:13:14 +0200

    MSF-289 - Rspamd auto-update.

---
+++ Definitions/main.mail_server/docker/anti_spam/1.0.0/Scripts/update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+memDb=$1
+memDbPort=$2
+memDbIp=$(sh /getip.sh "${memDb}")
+
+echo "Redis configuration: ${memDb}:${memDbPort}"
+echo "Updating Rspamd definitions: $(date)"
+echo "Redis configuration [2]: ${memDb}: ip= ${memDbIp}, port= ${memDbPort}"
+rm -f /var/lib/rspamd/*sqlite
+wget -P /var/lib/rspamd https://rspamd.com/rspamd_statistics/bayes.ham.sqlite
+wget -P /var/lib/rspamd https://rspamd.com/rspamd_statistics/bayes.spam.sqlite
+chown _rspamd._rspamd /var/lib/rspamd/*sqlite
+rspamadm statconvert --spam-db /var/lib/rspamd/bayes.spam.sqlite --symbol-spam BAYES_SPAM \
+--ham-db /var/lib/rspamd/bayes.ham.sqlite --symbol-ham BAYES_HAM -h \
+"${memDbIp}:${memDbPort}"
+
+sleep 43200; sh /update.sh "${memDb}" "${memDbPort}" &
+
--- Definitions/main.mail_server/docker/anti_virus/1.0.0/Scripts/do_clam.sh
+++ Definitions/main.mail_server/docker/anti_virus/1.0.0/Scripts/do_clam.sh
@@ -5,8 +5,8 @@ chown -R clamscan /var/lib/clamav
 chmod 770 /var/lib/clamav
 
 clamLog=$1
-freshclam >> ${clamLog}
-clamd >> ${clamLog}
-clamscan --infected --remove --recursive /target >> ${clamLog}
+freshclam >> "${clamLog}"
+clamd >> "${clamLog}"
+clamscan --infected --remove --recursive /target >> "${clamLog}"
 
 sleep 43200; sh /do_clam.sh &
