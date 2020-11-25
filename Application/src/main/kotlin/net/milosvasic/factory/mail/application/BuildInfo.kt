fb4831b6fda183c9527ed620e080ebea683f9b5e
Milos Vasic <milos85vasic@gmail.com>
2020-11-25 16:50:32 +0100

    MSF-355 - Build info.

--- Application/src/main/kotlin/net/milosvasic/factory/mail/application/BuildInfo.kt
+++ Application/src/main/kotlin/net/milosvasic/factory/mail/application/BuildInfo.kt
@@ -4,7 +4,7 @@ import net.milosvasic.factory.application.BuildInformation
 
 object BuildInfo : BuildInformation {
 
-    override val version = "1.0.0"
+    override val version = "1.0.0 Alpha 1"
     override val versionCode = (100 * 1000) + 0
     override val versionName = "Mail Server Factory Client"
 
