610795c5a04626ace61dcbbb64dac567f3d70775
Milos Vasic <milos85vasic@gmail.com>
2021-01-02 12:34:42 +0100

    MSF-399 - Stripping down repo(s).

--- Application/src/os/default/kotlin/net/milosvasic/factory/mail/application/OSInit.kt
+++
@@ -1,8 +0,0 @@
-package net.milosvasic.factory.mail.application
-
-import net.milosvasic.factory.log
-
-object OSInit : Runnable {
-
-    override fun run() = log.v("Starting")
-}
--- cleanup_logs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-LOGS="Logs"
-
-rm -rf "$LOGS"
-mkdir "$LOGS"
